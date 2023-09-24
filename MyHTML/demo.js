919eb349e8898af274315c032abc081a8c1b479f
Jestoni Cruz <jestoni.estrada.cruz@gmail.com>
2023-09-24 16:29:26 +0800

    Project Start-up

---
+++ MyHTML/demo.js
@@ -0,0 +1,22 @@
+function SignIn(){
+    console.log("SignIn button was clicked")
+    hello()
+    // var admin = document.getElementById("email").value;
+    // var password = document.getElementById("password").value;
+    // if(admin === "admin@gmail.com" && password == 12345){
+    //     document.getElementById("hello-world").innerHTML = "Successfully Login";
+    //     console.log("successful login")
+    // }
+    // else{
+    //     document.getElementById("hello-world").innerHTML = "Login failed";
+    //     console.log("login failed")
+    // }
+    
+}
+
+
+function alert(){
+    window.alert("SignIn button was clicked")
+}
+
+
---
+++ iCAREPH/demo2.js
@@ -0,0 +1,12 @@
+function togglemenu() {
+    var menulist = document.getElementById("menulist");
+    menulist.style.maxHeight = "0px";
+
+    if(menulist.style.maxHeight == "0px"){
+        menulist.style.maxHeight = "200px";
+    }
+    else{
+        menulist.style.maxHeight == "0px"
+    }
+
+}
